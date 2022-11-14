11629d8e7b34e5ec123a7b40e8f393f42e9e5fb1
Handy Kusuma <handy.kusuma@roketin.com>
2022-11-14 11:46:25 +0700

    updated pokedex

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -3,11 +3,21 @@ import { useState, useEffect } from "react";
 import ReactLoading from "react-loading";
 import axios from "axios";
 import Modal from "react-modal";
+import BarChart from "react-easy-bar-chart";
+
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 
 function PokeDex() {
   const [pokemons, setPokemons] = useState([]);
+  const [pokemonsFiltered, setPokemonsFiltered] = useState([]);
   const [pokemonDetail, setPokemonDetail] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [prevApi, setPrevApi] = useState("");
+  const [nextApi, setNextApi] = useState("");
+  const [apiUrl, setApiUrl] = useState("https://pokeapi.co/api/v2/pokemon");
 
   const customStyles = {
     content: {
@@ -23,7 +33,107 @@ function PokeDex() {
     overlay: { backgroundColor: "grey" },
   };
 
-  if (!isLoading && pokemons.length === 0) {
+  useEffect(() => {
+    // if (pokemons && pokemons.length === 0) {
+      setIsLoading(true);
+      fetchPokeDex()
+    // }
+  }, [apiUrl]);
+
+  function fetchPokeDex() {
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        const { results, next, previous } = res.data;
+        setPokemons(results);
+        setPokemonsFiltered(results);
+        setPrevApi(previous);
+        setNextApi(next);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+      });
+  }
+
+  function openModal(val) {
+    axios
+      .get(val.url)
+      .then((res) => {
+        console.log({ res });
+        setPokemonDetail(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function searchPokemon(val) {
+    setSearch(val);
+  }
+
+  function resolveDataToBarChart(datas) {
+    return datas.map((val) => ({
+      title: val.stat.name,
+      value: val.base_stat,
+      color: "blue",
+    }));
+  }
+
+  useEffect(() => {
+    let result = pokemons;
+    if (search) {
+      result = pokemons.filter((pokemon) => {
+        return pokemon.name.includes(search);
+      });
+    }
+    setPokemonsFiltered(result);
+  }, [search]);
+
+  function printDocument() {
+    const content = document.getElementsByTagName("BODY")[0];
+    const input = document.getElementById("divToPrint");
+    if (input) {
+      input.document.open();
+      input && input.document.write(content.innerHTML);
+      input.document.close();
+    }
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "JPEG", 0, 0);
+      pdf.save("downloadModal.pdf");
+    });
+  }
+
+  function sortPokemons() {
+    let results = [];
+    if (["", "DESC"].includes(sort)) {
+      results = [...pokemonsFiltered].sort((a, b) =>
+        a.name > b.name ? 1 : -1
+      );
+
+      // console.log(strAscending);
+      setSort("ASC");
+    } else {
+      results = [...pokemonsFiltered].sort((a, b) =>
+        a.name < b.name ? 1 : -1
+      );
+      // console.log(strDescending);
+      setSort("DESC");
+    }
+    setPokemonsFiltered(results);
+  }
+
+  function prev() {
+    setApiUrl(prevApi)
+  }
+
+  function next() {
+    setApiUrl(nextApi)
+  }
+
+  if (!isLoading && pokemons && pokemons.length === 0) {
     return (
       <div>
         <header className="App-header">
@@ -31,10 +141,13 @@ function PokeDex() {
           <h2>Requirement:</h2>
           <ul>
             <li>
-              Call this api:https://pokeapi.co/api/v2/pokemon to get pokedex, and show a list of pokemon name.
+              Call this api:https://pokeapi.co/api/v2/pokemon to get pokedex,
+              and show a list of pokemon name.
             </li>
             <li>Implement React Loading and show it during API call</li>
-            <li>when hover on the list item , change the item color to yellow.</li>
+            <li>
+              when hover on the list item , change the item color to yellow.
+            </li>
             <li>when clicked the list item, show the modal below</li>
             <li>
               Add a search bar on top of the bar for searching, search will run
@@ -42,7 +155,10 @@ function PokeDex() {
             </li>
             <li>Implement sorting and pagingation</li>
             <li>Commit your codes after done</li>
-            <li>If you do more than expected (E.g redesign the page / create a chat feature at the bottom right). it would be good.</li>
+            <li>
+              If you do more than expected (E.g redesign the page / create a
+              chat feature at the bottom right). it would be good.
+            </li>
           </ul>
         </header>
       </div>
@@ -56,27 +172,56 @@ function PokeDex() {
           <>
             <div className="App">
               <header className="App-header">
-                <b>Implement loader here</b>
+                <ReactLoading
+                  type={"balls"}
+                  color={"#3d3d3d"}
+                  height={667}
+                  width={375}
+                />
               </header>
             </div>
           </>
         ) : (
           <>
             <h1>Welcome to pokedex !</h1>
-            <b>Implement Pokedex list here</b>
+            {/* { pokemons } */}
+            <input
+              type="search"
+              name="search"
+              defaultValue=""
+              onKeyUp={(val) => searchPokemon(val.target.value)}
+            ></input>
+            <button onClick={() => sortPokemons()}>Sort</button>
+            {pokemonsFiltered &&
+              pokemonsFiltered.map((val, idx) => (
+                // <div class="card">
+                //   <div class="container">
+                <div
+                  key={idx}
+                  className="PokeList"
+                  onClick={() => openModal(val)}
+                >
+                  {val.name}
+                </div>
+                //   </div>
+                // </div>
+              ))}
+            <button onClick={() => prev()} disabled={ !prevApi }>Prev</button>
+            <button onClick={() => next()} disabled={ !nextApi }>Next</button>
           </>
         )}
       </header>
       {pokemonDetail && (
         <Modal
-          isOpen={pokemonDetail}
+          isOpen={!!pokemonDetail}
           contentLabel={pokemonDetail?.name || ""}
           onRequestClose={() => {
             setPokemonDetail(null);
           }}
           style={customStyles}
+          // appElement={el}
         >
-          <div>
+          {/* <div>
             Requirement:
             <ul>
               <li>show the sprites front_default as the pokemon image</li>
@@ -85,9 +230,38 @@ function PokeDex() {
                 required in tabular format
               </li>
               <li>Create a bar chart based on the stats above</li>
-              <li>Create a  buttton to download the information generated in this modal as pdf. (images and chart must be included)</li>
+              <li>
+                Create a buttton to download the information generated in this
+                modal as pdf. (images and chart must be included)
+              </li>
             </ul>
+          </div> */}
+          <div id="divToPrint">
+            <img
+              src={pokemonDetail.sprites.front_default}
+              alt="the sprites"
+            ></img>
+            <span>Stats Details</span>
+            <ul>
+              {pokemonDetail &&
+                pokemonDetail.stats &&
+                pokemonDetail.stats.map((valStat, idx) => (
+                  <li key={idx}>
+                    Base Stat : {valStat.base_stat}
+                    <br></br>
+                    Stat Name : {valStat.stat.name}
+                  </li>
+                ))}
+            </ul>
+            <BarChart
+              xAxis="stats"
+              yAxis="values"
+              height={400}
+              width={800}
+              data={resolveDataToBarChart(pokemonDetail.stats)}
+            />
           </div>
+          <button onClick={printDocument()}>Download as PDF</button>
         </Modal>
       )}
     </div>
